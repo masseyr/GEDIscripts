1a22bb2541300236822aae15e7949a2ee8996ad5
Richard Massey <rm885@nau.edu>
2020-04-18 22:54:58 -0700

    Update gedi_footprint.py

--- gedi_footprint.py
+++ gedi_footprint.py
@@ -22,7 +22,7 @@ python gedi_footprint.py [gedi directory] [output shapefile]
 
 
 def get_path(filename,
-             res=0.05,
+             res=0.5,
              buffer=0.000135):
     """
     Method to extract path from a GEDI file
@@ -37,17 +37,17 @@ def get_path(filename,
     year = int(date_str[0:4])
     julian_day = int(date_str[4:7])
 
-    bins = np.arange(-180.0, 180, res)
-    if bins[-1] < 180.0:
-        bins = np.hstack([bins, np.array([180.0])])
+    bin_edges = np.arange(-180.0, 180, res)
+    bin_widths = bin_edges[1:] - bin_edges[:-1]
 
-    coords = bins[:-1] + res / 2.0
-    if coords[-1] > 180.0:
-        coords[-1] = 180.0
+    if bin_edges[-1] < 180.0:
+        bin_edges = np.hstack([bin_edges, np.array([180.0])])
+
+    x_coords = (bin_edges[:-1] + bin_edges[1:]) / 2.0
 
     if buffer < res:  # if buffer is more than or eq to half bin width, ignore buffer
-        coords[0] = coords[0] + buffer
-        coords[-1] = coords[-1] - buffer
+        x_coords[0] = x_coords[0] + buffer
+        x_coords[-1] = x_coords[-1] - buffer
 
     file_keys = []
     try:
@@ -79,13 +79,13 @@ def get_path(filename,
 
         pos_arr = pos_arr[np.lexsort((pos_arr[:, 0], pos_arr[:, 1]))]
 
-        upper_lims, _, _ = binned_statistic(pos_arr[:, 0], pos_arr[:, 1], statistic='max', bins=bins)
-        lower_lims, _, _ = binned_statistic(pos_arr[:, 0], pos_arr[:, 1], statistic='min', bins=bins)
+        upper_lims, _, _ = binned_statistic(pos_arr[:, 0], pos_arr[:, 1], statistic='max', bins=bin_edges)
+        lower_lims, _, _ = binned_statistic(pos_arr[:, 0], pos_arr[:, 1], statistic='min', bins=bin_edges)
 
         nan_loc = np.where(np.isnan(upper_lims))
         nan_groups = group_consecutive(nan_loc[0].tolist())
 
-        chunks = locate_slice_by_group(nan_groups, coords.shape[0])
+        chunks = locate_slice_by_group(nan_groups, x_coords.shape[0])
 
         main_geom = ogr.Geometry(ogr.wkbMultiPolygon)
 
@@ -93,12 +93,21 @@ def get_path(filename,
             if chunk[0] >= chunk[1]:
                 continue
             else:
-                upper_bounds = np.vstack([coords[chunk[0]:chunk[1]], upper_lims[chunk[0]:chunk[1]]])
-                lower_bounds = np.vstack([coords[chunk[0]:chunk[1]], lower_lims[chunk[0]:chunk[1]]])
+                chunk_bin_widths = bin_widths[chunk[0]:chunk[1]]
 
-                lower_bounds = np.flip(lower_bounds, 1)
+                upper_bounds = np.vstack([x_coords[chunk[0]:chunk[1]],
+                                          upper_lims[chunk[0]:chunk[1]]])
 
-                part_geom_coords = np.hstack([upper_bounds, lower_bounds]).T
+                lower_bounds = np.vstack([x_coords[chunk[0]:chunk[1]],
+                                          lower_lims[chunk[0]:chunk[1]]])
+
+                corr_upper_bounds, corr_lower_bounds = correct_bounds(upper_bounds,
+                                                                      lower_bounds,
+                                                                      chunk_bin_widths)
+
+                corr_lower_bounds = np.flip(corr_lower_bounds, 1)
+
+                part_geom_coords = np.hstack([corr_upper_bounds, corr_lower_bounds]).T
                 part_geom_coords = np.vstack([part_geom_coords, part_geom_coords[0, :]])
 
                 part_geom_json = json.dumps({'type': 'Polygon', 'coordinates': [part_geom_coords.tolist()]})
@@ -121,6 +130,50 @@ def get_path(filename,
         return Handler(filename).basename, err
     else:
         return feat_list, None
+
+
+def correct_bounds(u_bounds,
+                   l_bounds,
+                   bin_widths):
+    """
+    Method to correct upper and lower bounds of GEDI beam path
+    The correction is most needed when he path is least parallel to equator and vice versa
+    :param u_bounds: upper bounds of beam path : array([[x1, x2, ....],
+                                                        [y1, y2, ....]])
+    :param l_bounds: lower bounds of beam path : array([[x1, x2, ....],
+                                                        [y1, y2, ....]])
+    :param bin_widths: bin widths of type array([b1, b2, ...])
+    :return: tuple of (corrected upper bounds, corrected lower bounds)
+    """
+
+    if u_bounds.shape[1] <= 1:
+        return u_bounds, l_bounds
+
+    else:
+        y_diff = (u_bounds[1, :] - l_bounds[1, :]) / 2.0
+        x_diff = bin_widths / 2.0
+
+        centr_coords = (u_bounds + l_bounds) / 2.0
+
+        theta = np.arctan((u_bounds[1, :-1] - u_bounds[1, 1:]) /
+                          (u_bounds[0, :-1] - u_bounds[0, 1:]))
+
+        theta = np.hstack([theta, np.array([theta[-1]])])
+
+        tan_theta = np.abs(np.tan(theta))
+        cos_theta = np.cos(theta)
+        sin_theta = np.sin(theta)
+
+        y_corr = (y_diff - (x_diff * tan_theta)) * (cos_theta ** 2)
+        x_corr = (y_diff - (x_diff * tan_theta)) * (cos_theta * sin_theta)
+
+        corr = np.vstack([x_corr,
+                          y_corr])
+
+        new_u_bounds = centr_coords + corr
+        new_l_bounds = centr_coords - corr
+
+        return new_u_bounds, new_l_bounds
 
 
 def group_consecutive(arr):
